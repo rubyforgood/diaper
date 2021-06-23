3a1210ff513250b8fb5a7a534395feac9d466eb4
Albert Chae <albertchae@users.noreply.github.com>
2021-06-22 23:06:20 -0700

    change text to jsonb in generated migrations
    We want to do this for easier querying with sql.
    
    https://github.com/paper-trail-gem/paper_trail#postgresql-json-column-type-support
    
    This commit runs the migration and saves the schema changes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_517_000_207) do
+ActiveRecord::Schema.define(version: 20_210_612_212_117) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -417,6 +417,17 @@ ActiveRecord::Schema.define(version: 20_210_517_000_207) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index %w(latitude longitude), name: "index_vendors_on_latitude_and_longitude"
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.bigint "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.jsonb "object"
+    t.datetime "created_at"
+    t.jsonb "object_changes"
+    t.index %w(item_type item_id), name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
